02141c2ac8819a4351bcd08a61e252bff8a7741d
LondekaNduli <londekahza@gmail.com>
2026-05-20 09:05:10 +0200

    Delete salesforcasting.py

--- salesforcasting.py
+++
@@ -1,9 +0,0 @@
-#Import libraries
-import pandas as pd
-import plotly.express as px
-import numpy as np
-
-#Read file
-df = pd.read_csv("salesforcasting.csv", sep=";")
-print(df.head())
-print(df.shape)
